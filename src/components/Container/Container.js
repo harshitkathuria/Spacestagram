a5f129b6ec2167774e8c6ac59d1c3f97d36fb673
harshitkathuria <harshitkathuria14@gmail.com>
2022-01-19 21:56:32 +0530

    Add Github Icon

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useContext } from "react";
+import { FaGithub } from "react-icons/fa";
 import "./Container.css";
 import AppContext from "../../context/AppContext";
 import Loader from "../Loader/Loader";
@@ -21,7 +22,16 @@ const Container = () => {
   return (
     <div className="container">
       <nav className="navbar">
-        <div className="banner">SPACESTAGRAM</div>
+        <div className="banner">
+          <span>SPACESTAGRAM</span>
+          <a
+            id="github"
+            target={"_blank"}
+            href="https://github.com/harshitkathuria/Spacestagram"
+          >
+            <FaGithub />
+          </a>
+        </div>
         <div id="social-links"></div>
       </nav>
       {loading ? <Loader /> : <Grid images={images} />}
